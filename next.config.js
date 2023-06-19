5386b3351a192344c18c35b62efc0f45a64d625d
sunlyhuor <jkhmer1111@gmail.com>
2023-06-19 22:11:20 +0700

    config nextconfig

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
         port: '3000',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'school-ms-backend.onrender.com',
+        // port: '3000',
+        pathname: '/**',
+      },
     ],
   },
   reactStrictMode: true,
